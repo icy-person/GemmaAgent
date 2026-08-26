e03afa5da0a41670add0f3b32446b7513592f720
icy-person <kar.fars.1388@gmail.com>
2026-08-26 09:05:40 +0330

    fix: migrate shared KMP module to Android KMP library plugin

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,22 +1,22 @@
 plugins {
     id("org.jetbrains.kotlin.multiplatform")
     id("org.jetbrains.kotlin.plugin.serialization")
-    id("com.android.library")
+    id("com.android.kotlin.multiplatform.library")
 }
 
 kotlin {
-    androidTarget()
+    android {
+        namespace = "com.example.gemmaagent.shared"
+        compileSdk = 36
+        minSdk = 26
+    }
+
     jvm()
+
     sourceSets {
         commonMain.dependencies {
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
         }
     }
-}
-
-android {
-    namespace = "com.example.gemmaagent.shared"
-    compileSdk = 36
-    defaultConfig { minSdk = 26 }
 }
