1eaa3840fb73da70763a928f1a6f489d9084aa6e
icy-person <kar.fars.1388@gmail.com>
2026-08-26 08:58:30 +0330

    Add Kotlin Compose compiler plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,6 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("org.jetbrains.compose") version "1.9.0" apply false
 }
