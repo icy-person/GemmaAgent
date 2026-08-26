d3ff320319232c28687dca73a68a51dab3703d1b
icy-person <kar.fars.1388@gmail.com>
2026-08-26 12:54:14 +0330

    Add shared runtime smoke-test support

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,5 +18,9 @@ kotlin {
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
         }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+        }
     }
 }
