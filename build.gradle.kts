03386f4959478d63051a9a0b8ed5fb15bfadd5a7
icy-person <kar.fars.1388@gmail.com>
2026-08-26 09:05:33 +0330

    fix: add Android KMP library plugin for AGP 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application") version "9.3.0" apply false
     id("com.android.library") version "9.3.0" apply false
+    id("com.android.kotlin.multiplatform.library") version "9.3.0" apply false
     id("org.jetbrains.kotlin.android") version "2.4.10" apply false
     id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.4.10" apply false
