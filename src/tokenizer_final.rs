e76541b9e59863f3ef01a344beb46e9f65be1ca2
icy-person <kar.fars.1388@gmail.com>
2026-09-07 16:02:38 +0330

    Add byte tokenizer and training corpus loader

---
+++ src/tokenizer_final.rs
@@ -0,0 +1,4 @@
+pub const BOS:usize=256;pub const EOS:usize=257;
+pub struct Tokenizer;
+impl Tokenizer{pub fn new()->Self{Self}pub fn encode(&self,s:&str)->Vec<usize>{let mut v=Vec::with_capacity(s.len()+2);v.push(BOS);v.extend(s.bytes().map(|b|b as usize));v.push(EOS);v}pub fn decode(&self,t:&[usize])->String{String::from_utf8_lossy(&t.iter().filter_map(|&x|if x<256{Some(x as u8)}else{None}).collect::<Vec<_>>()).into_owned()}}
+pub fn tiny_corpus()->String{("Rust is a systems programming language. ".repeat(64))+"A small language model learns to predict the next token. Rust makes the engine fast and explicit. ".repeat(64)}
