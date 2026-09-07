6ce0779d6643a0a7ad6a9e04e31f8e736983718d
icy-person <kar.fars.1388@gmail.com>
2026-09-07 17:53:42 +0330

    Add global gradient clipping to AdamW

--- src/optim.rs
+++ src/optim.rs
@@ -8,6 +8,7 @@ pub struct AdamW {
     beta2: f32,
     eps: f32,
     weight_decay: f32,
+    max_grad_norm: f32,
     m: HashMap<usize, Vec<f32>>,
     v: HashMap<usize, Vec<f32>>,
 }
@@ -22,6 +23,7 @@ impl AdamW {
             beta2: 0.999,
             eps: 1e-8,
             weight_decay: 0.01,
+            max_grad_norm: 1.0,
             m: HashMap::new(),
             v: HashMap::new(),
         }
@@ -31,6 +33,21 @@ impl AdamW {
         self.t += 1;
         let bias1 = 1.0 - self.beta1.powi(self.t as i32);
         let bias2 = 1.0 - self.beta2.powi(self.t as i32);
+
+        let mut global_norm_sq = 0.0f32;
+        for parameter in parameters {
+            global_norm_sq += parameter
+                .grad()
+                .iter()
+                .map(|g| g * g)
+                .sum::<f32>();
+        }
+        let global_norm = global_norm_sq.sqrt();
+        let clip_scale = if global_norm > self.max_grad_norm {
+            self.max_grad_norm / global_norm
+        } else {
+            1.0
+        };
 
         for parameter in parameters {
             let id = parameter.id();
@@ -49,8 +66,9 @@ impl AdamW {
 
             let mut next = data.clone();
             for i in 0..data.len() {
-                m[i] = self.beta1 * m[i] + (1.0 - self.beta1) * grad[i];
-                v[i] = self.beta2 * v[i] + (1.0 - self.beta2) * grad[i] * grad[i];
+                let clipped_grad = grad[i] * clip_scale;
+                m[i] = self.beta1 * m[i] + (1.0 - self.beta1) * clipped_grad;
+                v[i] = self.beta2 * v[i] + (1.0 - self.beta2) * clipped_grad * clipped_grad;
                 let m_hat = m[i] / bias1;
                 let v_hat = v[i] / bias2;
                 next[i] *= 1.0 - self.lr * self.weight_decay;
@@ -74,6 +92,18 @@ mod tests {
         let mut opt = AdamW::new(0.01);
         opt.step(std::slice::from_ref(&p));
         assert!(p.data()[0] < 1.0);
+        assert_eq!(p.grad(), vec![0.0]);
+    }
+
+    #[test]
+    fn clipping_limits_a_large_gradient() {
+        let p = Value::leaf(1, 1, vec![0.0]);
+        let scale = Value::leaf(1, 1, vec![1000.0]);
+        let loss = p.mul(&scale);
+        loss.backward();
+        let mut opt = AdamW::new(0.01);
+        opt.step(std::slice::from_ref(&p));
+        assert!(p.data()[0].abs() <= 0.011);
         assert_eq!(p.grad(), vec![0.0]);
     }
 }
